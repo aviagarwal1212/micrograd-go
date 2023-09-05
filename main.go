5e27ab80fa88dcece39895e16a950f010b8da669
Avi Agarwal <aviagarwal1212@gmail.com>
2023-09-05 07:49:38 +0000

    implement label and operations

--- main.go
+++ main.go
@@ -8,11 +8,33 @@ import (
 func main() {
 	fmt.Println("Welcome to micrograd!")
 
-	a := micrograd.NewValue(2.0)
-	b := micrograd.NewValue(-3.0)
-	c := micrograd.NewValue(10.0)
-	d := a.Mul(b).Add(c)
+	a := micrograd.NewValue(2.0, "a")
+	b := micrograd.NewValue(-3.0, "b")
+	c := micrograd.NewValue(10.0, "c")
+	e := a.Mul(b, "e")
+	d := e.Add(c, "d")
+	f := micrograd.NewValue(-2.0, "f")
+	L := d.Mul(f, "L")
 
-	fmt.Printf("%s", d.String())
-	d.Previous()
+	L.Describe()
+
+	// inputs x1 and x2
+	x1 := micrograd.NewValue(2.0, "x1")
+	x2 := micrograd.NewValue(0.0, "x2")
+
+	// weights w1 and w2
+	w1 := micrograd.NewValue(-3.0, "w1")
+	w2 := micrograd.NewValue(1.0, "w2")
+
+	// bias
+	b = micrograd.NewValue(6.7, "b")
+
+	// x1*w1 + x2*w2 + b
+	x1w1 := x1.Mul(w1, "x1*w1")
+	x2w2 := x2.Mul(w2, "x2*w2")
+	x1w1x2w2 := x1w1.Add(x2w2, "x1*w1 + x2*w2")
+	n := x1w1x2w2.Add(b, "n")
+	o := n.Tanh("o")
+	o.Describe()
+
 }
--- micrograd/nn.go
+++ micrograd/nn.go
@@ -1,34 +1,54 @@
 package micrograd
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Value struct {
 	Data     float64
+	grad     float64
+	label    string
 	previous []Value
+	op       string
 }
 
-func NewValue(x float64) *Value {
-	return &Value{Data: x, previous: []Value{}}
+func NewValue(x float64, label string) *Value {
+	return &Value{Data: x, grad: 0.0, label: label, previous: []Value{}, op: ""}
 }
 
 func (value Value) String() string {
-	return fmt.Sprintf("Value ( Data: %.3f )\n", value.Data)
+	return fmt.Sprintf("Value '%s'( Data: %.3f )\n", value.label, value.Data)
 }
 
-func (value Value) Previous() {
-	printout := "Previous:\n"
+func (value Value) Describe() {
+	printout := value.String()
+	printout += fmt.Sprintf("Grad: %.3f\n", value.grad)
+	printout += "Previous:\n"
 	for _, child := range value.previous {
 		printout += child.String()
 	}
+	printout += fmt.Sprintf("Op: %s\n", value.op)
 	fmt.Printf("%s", printout)
 }
 
-func (value Value) Add(other *Value) *Value {
+func (value Value) Add(other *Value, label string) *Value {
 	out := value.Data + other.Data
-	return &Value{Data: out, previous: []Value{value, *other}}
+	return &Value{Data: out, label: label, previous: []Value{value, *other}, op: "+"}
 }
 
-func (value Value) Mul(other *Value) *Value {
+func (value Value) Mul(other *Value, label string) *Value {
 	out := value.Data * other.Data
-	return &Value{Data: out, previous: []Value{value, *other}}
+	return &Value{Data: out, label: label, previous: []Value{value, *other}, op: "*"}
+}
+
+func (value Value) Tanh(label string) *Value {
+	n := value.Data
+	t := (math.Exp(2*n) - 1) / (math.Exp(2*n) + 1)
+	return &Value{
+		Data:     t,
+		label:    label,
+		previous: []Value{value},
+		op:       "tanh",
+	}
 }
